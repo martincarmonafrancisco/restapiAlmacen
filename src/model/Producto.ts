d072b6e7657723be6b46321f963f65571fc9a5c9
Francisco Martin Carmona <fmartincarmona19@iespuntadelverde.es>
2021-02-18 16:45:41 +0100

    first commit

--- src/model/Producto.ts
+++ src/model/Producto.ts
@@ -2,6 +2,7 @@ import {Schema, model } from 'mongoose'
 import { identificacionRoutes } from '../routes/identificacionRoutes'
 // Definimos el Schema
 const productoSchema = new Schema({
+    _id: Number,
     _nombre: String,
     _precio: Number,
     _tipo: String,
